8b5f51dc0072351b8c41b7426659aa6a950d1603
Collin Sellman <collin.sellman@gmail.com>
2023-09-18 11:24:17 -0700

    change;

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from viam.components.sensor import Sensor
 from viam.resource.registry import Registry, ResourceCreatorRegistration
-from .wifi_sensor import MySensor
+from .gas_sensor import MySensor
 
 
 Registry.register_resource_creator(Sensor.SUBTYPE, MySensor.MODEL, ResourceCreatorRegistration(MySensor.new))
